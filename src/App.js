247c39657bf83ff4ce2691fb4548bc136ecfd14e
ihsanharimurti <harimurti114@gmail.com>
2025-03-07 21:52:13 +0700

    Edit Web

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fact: 'Click the button to see a random fact!'
+    };
+  }
+
+  facts = [
+    'Bananas are berries, but strawberries are not.',
+    'Honey never spoils.',
+    'Octopuses have three hearts.',
+    'A day on Venus is longer than a year on Venus.',
+    'Water can boil and freeze at the same time.'
+  ];
+
+  handleClick = () => {
+    const randomFact = this.facts[Math.floor(Math.random() * this.facts.length)];
+    this.setState({ fact: randomFact });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React123!</h1>
+          <h1 className="App-title">Welcome to React!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.fact}</p>
+        <button onClick={this.handleClick}>Show Random Fact</button>
       </div>
     );
   }
